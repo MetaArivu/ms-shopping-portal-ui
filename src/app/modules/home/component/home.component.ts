20a9f1b08efb4177d3b56e2d8ac6c2beeb7acfb7
ketangote <ketan.gote@metamagic.in>
2021-12-01 20:26:20 +0530

    Changes: Added Home Page

--- src/app/modules/home/component/home.component.ts
+++ src/app/modules/home/component/home.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Menu } from "../model/menu";
 
 @Component({
     selector : 'home',
-    templateUrl : './home.component.html'
+    templateUrl : './home.component.html',
+    styleUrls:['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private route: Router) {
+    menus : Menu[] = [];
 
+    constructor(private router: Router) {
+
+        this.menus = [];
+        this.menus.push(new Menu("Catalog","category","/catalog"));
+        this.menus.push(new Menu("My Cart","shopping_cart","/mycart"));
+        this.menus.push(new Menu("My Orders","list_alt","/myorders" ));
+        
     }
 
     ngOnInit() {
 
+    }
+
+    onMenuClick(menu: Menu){
+        console.log(menu);
+        this.router.navigate([menu.routeLink]);
+    }
+
+    logout(){
+        this.router.navigate(['/']);
     }
 }
