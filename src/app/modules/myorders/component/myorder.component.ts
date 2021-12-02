45b3d4935543864b265ca7d1efdcba9ed57bc411
ketangote <ketan.gote@metamagic.in>
2021-12-02 21:42:04 +0530

    Changes: Mock Order UI..

--- src/app/modules/myorders/component/myorder.component.ts
+++ src/app/modules/myorders/component/myorder.component.ts
@@ -4,25 +4,37 @@ import { MyOrderService } from "../services/myorder.service";
 
 @Component({
     selector : 'myorder',
-    templateUrl : './myorder.component.html'
+    templateUrl : './myorder.component.html',
+    styleUrls : ['./myorder.component.css']
 })
 export class MyOrderComponent implements OnInit {
 
     myOrder: MyOrderModel[];
+    displayedColumns: string[] = ['orderNo', 'orderDate','success','total'];
+    totalAmount: number;
 
     constructor(private myOrderService: MyOrderService) {
         this.myOrder = [];
+        this.totalAmount = 0;
     }
 
     ngOnInit(){
-        this.fetchMyCartDetails();
+        this.fetchMyOrderDetails();
     }
 
-    private fetchMyCartDetails(){
+    private fetchMyOrderDetails(){
+        this.totalAmount = 0;
+        this.myOrder = [];
         this.myOrderService
             .fetchMyOrders()
-            .subscribe((myOrder: MyOrderModel[])=>{
-                this.myOrder = myOrder;
+            .subscribe((_myOrder: MyOrderModel[])=>{
+                _myOrder.forEach((order)=>{
+                    let model : MyOrderModel  = new MyOrderModel(order.id,order.orderNo, order.orderDate,order.total,order.success);
+                    this.totalAmount = this.totalAmount + model.total;
+                    this.myOrder.push(model);
+                });
+                console.log(this.myOrder);
+                //this.myOrder = myOrder;
             })
     }
 }
