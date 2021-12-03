d3c641fe2cb24e48331dc513f2033824958fa14a
ketangote <ketan.gote@metamagic.in>
2021-12-03 15:27:51 +0530

    Changes: Added Account creation screen..

--- src/app/modules/user/component/add.user.component.ts
+++ src/app/modules/user/component/add.user.component.ts
@@ -20,7 +20,19 @@ export class AddUserComponent implements OnInit {
 
         this.userForm = new FormGroup({
             userName: new FormControl(null, [Validators.required, Validators.email]),
-            password: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)])
+            password: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
+            confirmPassword: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
+
+            firstName: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+            middleName: new FormControl(null, []),
+            lastName: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            phone: new FormControl(null, [Validators.required]),
+
+            dob: new FormControl(null, [Validators.required]),
+            gender: new FormControl(null, [Validators.required]),
+
         });
     }
 
@@ -28,7 +40,22 @@ export class AddUserComponent implements OnInit {
 
     }
 
-    onSubmit(){
-        
+    onSubmit() {
+        if (!this.userForm.valid) {
+            this._snackBar.open("Please enter valid data for account creation!", "Error", { duration: 2000 })
+        } else {
+            this.loader = true;
+            setTimeout(() => {
+                this.router.navigate(['/']);
+                console.log(this.userForm.value);
+                this._snackBar.open("Account created successfully!", "Success", { duration: 2000 })
+
+            }, 1000);
+        }
+
+    }
+
+    reset() {
+        this.userForm.reset();
     }
 }
