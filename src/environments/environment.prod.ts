b666cf073221172f0a0eae9a094dd5bb6bbd1914
deepali-arvind <deepali.arvind@metamagic.in>
2021-12-29 13:05:58 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,31 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true
+  production: true,
+  
+  userService : "/user-svc",
+  prodService : "/product-service",
+  cartCmdService : "/shopping-cart-cmd-service",
+  cartQueryService : "/shopping-cart-query-service",
+  paymentService : "/payment-service",
+  orderService : "/order-service",
+
+  loginUrl : "/api/v1/login",
+  registerUserUrl : "/api/v1/register",
+  loginUserInfoUrl : "/api/v1/login/userinfo",
+
+  allProducts : "/api/v1/",
+
+  addToCart : "/api/v1/add",
+  removeFromCart : "/api/v1/remove",
+  checkout: "/api/v1/checkout",
+  
+  myCart : "/api/v1/",
+
+  makePayment : "/api/v1/",
+
+  orderByUserId : "/api/v1/",
+  checkOutOrderByUserId : "/api/v1/checkout"
 };
